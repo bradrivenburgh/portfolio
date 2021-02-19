64fc6d5050b421e2cb5227ec2e66ffb9b2356362
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-02-19 13:05:08 -0500

    revised portfolio so that it does not use tiles that hide project description text

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// JavaScript goes here
+/**
+ * Used to expand project tiles when tile styles are enabled;
+ * @param {object} e 
+ */
 
 function expandProject(e) {
   const element = document.getElementById(e.target.alt)
