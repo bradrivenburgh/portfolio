61e1a7f87fc6bd9bd774f1a5569b435cf588ad1b
Brad Rivenburgh <brad.rivenburgh@gmail.com>
2021-04-08 08:07:19 -0500

    cleaned up classes and scripts

--- script.js
+++
@@ -1,9 +0,0 @@
-/**
- * Used to expand project tiles when tile styles are enabled;
- * @param {object} e 
- */
-
-function expandProject(e) {
-  const element = document.getElementById(e.target.alt)
-  element.classList.toggle('expand')
-}
